e6a6b717b7231366c373d1c96d3385c8cd7cea86
Ishita <ishita.sri001@gmail.com>
2025-06-16 15:37:53 +0530

    button animations completed

--- src/components/Buttons/Link.jsx
+++ src/components/Buttons/Link.jsx
@@ -4,9 +4,11 @@ import Image from 'next/image'
 
 const LinkBtn = () => {
   return (
-    <div className='w-[40vw] h-[12vh] cursor-pointer'>
+    <div className='w-[40vw] h-[12vh] cursor-pointer group relative overflow-hidden'>
 
-    <div className='flex items-center justify-between relative'>
+    <div className='flex flex-col translate-y-[-50%] group-hover:translate-y-[0] duration-500 transition-all'>
+      <div className='flex items-center justify-between  '>
+
         <div className='w-[3vw] h-[5vh]'>
             <Image
                 src='/assets/icons/enter.svg'
@@ -16,11 +18,30 @@ const LinkBtn = () => {
                 className='h-full w-full object-fill'
                 />
         </div>
-        <Link href='./' className='text-[4.5vw] text-primary w-full'> Explore Collection </Link>
-        <div className='w-[38vw] px-[1vw] h-[0.3vw] absolute bottom-[10%] left-[1%] bg-black'>
+        <Link href='./' className='text-[4.5vw] text-primary w-full text-nowrap'> Explore Collection </Link>
+          
+        </div>
+        <div className='flex items-center justify-between  group'>
+
+        <div className='w-[3vw] h-[5vh]'>
+            <Image
+                src='/assets/icons/enter.svg'
+                width={900}
+                height={900}
+                alt='arrow'
+                className='h-full w-full object-fill'
+                />
+        </div>
+        <Link href='./' className='text-[4.5vw] text-primary w-full text-nowrap'> Explore Collection </Link>
 
         </div>
+          
+
+        
+        
+                
         </div>
+         <div className='w-[38vw] px-[1vw] h-[0.3vw] absolute bottom-0 left-[1%] bg-black' />
     </div>
   )
 }
